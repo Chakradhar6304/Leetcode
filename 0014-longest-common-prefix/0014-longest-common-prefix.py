3f3fa2377a3df5c2c0b298a72dea54ebf71feda0
Chakradhar Reddy Nemalidinne <146489212+Chakradhar6304@users.noreply.github.com>
2025-03-01 19:25:49 -0500

    Time: 0 ms (100%), Space: 12.3 MB (91.86%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -6,17 +6,14 @@ class Solution(object):
         """
         if not strs:
             return ""
+    
         prefix = strs[0]
         for string in strs[1:]:
-            temp_prefix = ""
-            
-            for i in range(min(len(prefix), len(string))):
-                if prefix[i] == string[i]:
-                    temp_prefix += prefix[i]
-                else:
-                    break
-            prefix = temp_prefix
-            if not prefix:
-                break
+            while not string.startswith(prefix):
+                prefix = prefix[:-1]  
+                if not prefix:
+                    return ""
 
         return prefix
+
+        
