ef3b6fa56b93daecf6ea8d147860946203cf469d
Chakradhar Reddy Nemalidinne <146489212+Chakradhar6304@users.noreply.github.com>
2025-08-19 15:22:59 -0400

    Time: 0 ms (100%), Space: 18 MB (17.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,43 +1,22 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 
 class Solution:
-    def mergeTwoLists(self, list1, list2):
-        # Creating a dummy node to start the merged list
-        dummy = ListNode()
-        current = dummy
-        
-        # Iterating through both lists
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1)   
+        tail = dummy           
+
         while list1 and list2:
-            # Comparing the values of the current nodes of list1 and list2
-            if list1.val < list2.val:
-                current.next = list1
+            if list1.val <= list2.val:
+                tail.next = list1
                 list1 = list1.next
             else:
-                current.next = list2
+                tail.next = list2
                 list2 = list2.next
-            current = current.next
-        
-        # Appending the remaining elements of list1 or list2 to the merged list
-        if list1:
-            current.next = list1
-        else:
-            current.next = list2
-        
-        # Returning the merged list, excluding the dummy node
+            tail = tail.next
+        tail.next = list1 if list1 else list2
+
         return dummy.next
